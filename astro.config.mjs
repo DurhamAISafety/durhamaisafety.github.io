d1f7b976aead1f5a238d979f2fba1f80838f2c49
Theosdoor <theo.farrell99@outlook.com>
2026-05-16 10:28:49 +0100

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import tina from '@tinacms/astro/integration';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://durhamaisafety.uk',
-  integrations: [sitemap()],
+  integrations: [sitemap(), tina()],
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'static',
+  output: 'server',
 });
