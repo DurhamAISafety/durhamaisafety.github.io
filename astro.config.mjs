fedc923e61796c1ce27cf3df0cd0dd00bd4edf19
Theosdoor <theo.farrell99@outlook.com>
2026-05-23 17:55:27 +0100

    fix: update site URL and downgrade @astrojs/netlify dependency version

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL,
+  site: "https://durhamaisafety.uk/",
   integrations: [sitemap(), tina()],
   adapter: netlify(),
   vite: {
